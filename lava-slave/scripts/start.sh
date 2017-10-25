905a723528e499e9eae193b4d72388076d561f6c
Corentin LABBE <clabbe@baylibre.com>
2017-10-25 13:45:56 +0200

    Add support for conmux

--- lava-slave/scripts/start.sh
+++ lava-slave/scripts/start.sh
@@ -10,5 +10,14 @@ service tftpd-hpa start || exit 4
 rm -f /var/run/lava-slave.pid
 service lava-slave start || exit 5
 
+touch /var/run/conmux-registry
+/usr/sbin/conmux-registry 63000 /var/run/conmux-registry&
+sleep 2
+for item in $(ls /etc/conmux/*cf)
+do
+	echo "Add $item"
+	/usr/sbin/conmux $item &
+done
+
 # start an http file server for boot/transfer_overlay support
 (cd /var/lib/lava/dispatcher; python -m SimpleHTTPServer 80)
