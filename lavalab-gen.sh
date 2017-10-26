1ba87d4097e4da3977f53ffd4fe808a3b0ec2c72
Corentin LABBE <clabbe@baylibre.com>
2017-10-26 10:58:47 +0200

    Add sample dhcpd and helper lavalab-gen.sh

---
+++ lavalab-gen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm -rv lava-master/devices/
+rm -rv lava-master/slaves/
+rm -rv lava-slave/conmux/
+rm -rv lava-master/tokens/
+rm -rv lava-master/users/
+rm lavalab*rules
+
+if [ "$1" = "mrproper" ];then
+	exit 0
+fi
+
+./lavalab-gen.py || exit 1
+
+rm /etc/udev/rules.d/lavalab*rules
+cp lavalab*rules /etc/udev/rules.d/
+
+docker-compose build || exit 1
+docker-compose up || exit 1
