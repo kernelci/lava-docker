a0d41c40d13fed10aa569b09241e7b4e149b0cc9
Corentin LABBE <clabbe@baylibre.com>
2017-11-07 16:03:20 +0100

    Add a better explaination on udev rules
    In the same time, start the filename with a number like others udev
    rules

--- lavalab-gen.sh
+++ lavalab-gen.sh
@@ -5,7 +5,7 @@ rm -rv lava-master/slaves/
 rm -rv lava-slave/conmux/
 rm -rv lava-master/tokens/
 rm -rv lava-master/users/
-rm lavalab*rules
+rm *lavalab*rules
 
 if [ "$1" = "mrproper" ];then
 	exit 0
@@ -13,8 +13,8 @@ fi
 
 ./lavalab-gen.py || exit 1
 
-rm /etc/udev/rules.d/lavalab*rules
-cp lavalab*rules /etc/udev/rules.d/
+rm /etc/udev/rules.d/*lavalab*rules
+cp *lavalab*rules /etc/udev/rules.d/
 
 docker-compose build || exit 1
 docker-compose up || exit 1
