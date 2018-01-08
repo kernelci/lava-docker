93222fc2af169e07c151728497832547eabc3614
Corentin LABBE <clabbe@baylibre.com>
2018-01-08 13:50:01 +0100

    Remove old squid PID
    Sometimes the old squid PID prevent squid from starting.

--- squid/entrypoint.sh
+++ squid/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -e /var/run/squid.pid ];then
+	#echo "DEBUG: Removed old squid PID"
+	rm /var/run/squid.pid
+fi
+
 # Create cache FS
 if [ ! -e /var/spool/squid/00 ];then
 	squid -z || exit $?
