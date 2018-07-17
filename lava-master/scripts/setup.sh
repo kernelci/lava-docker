0f09e5c9b89cee21a6ee39db9daf8e17525dd493
Corentin LABBE <clabbe@baylibre.com>
2018-07-17 13:30:43 +0200

    Do not add already existing token
    When restoring a database, token are restored and so cannot be re-created.

--- lava-master/scripts/setup.sh
+++ lava-master/scripts/setup.sh
@@ -70,8 +70,13 @@ if [ -e /root/lava-callback-tokens ];then
 			echo "Missing DESCRIPTION for $USER"
 			exit 1
 		fi
-		echo "Adding $USER ($DESCRIPTION) DEBUG($TOKEN)"
-		lava-server manage tokens add --user $USER --secret $TOKEN --description "$DESCRIPTION" || exit 1
+		lava-server manage tokens list --user $USER |grep -q $TOKEN
+		if [ $? -eq 0 ];then
+			echo "SKIP already present token for $USER"
+		else
+			echo "Adding $USER ($DESCRIPTION) DEBUG($TOKEN)"
+			lava-server manage tokens add --user $USER --secret $TOKEN --description "$DESCRIPTION" || exit 1
+		fi
 	done
 fi
 
