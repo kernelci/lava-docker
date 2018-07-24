80195a67349f8d64e2e42218ae6f2c165b50ecc8
Corentin LABBE <clabbe@baylibre.com>
2018-07-24 16:43:44 +0200

    lavalab-gen.sh: use rm -f

--- lavalab-gen.sh
+++ lavalab-gen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm lava-master/scripts/build-lava
-rm lava-slave/scripts/build-lava
-rm -r output
+rm -f lava-master/scripts/build-lava
+rm -f lava-slave/scripts/build-lava
+rm -rf output
 
 if [ "$1" = "mrproper" ];then
 	exit 0
