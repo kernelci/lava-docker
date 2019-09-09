a144f4de932d1d8925c3f76579ddd0f3979b9b3d
Christoph Rahn <crahn@rosen-group.com>
2019-09-09 10:34:20 +0200

    lava-slave: Use python3 xmlrpc

--- lava-slave/scripts/setdispatcherip.py
+++ lava-slave/scripts/setdispatcherip.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import xmlrpclib
+import xmlrpc.client
 import sys
 
 if len(sys.argv) < 4:
     print("ERROR: Usage: %s URI workername dispatcherIP" % sys.argv[0])
     sys.exit(1)
 
-server = xmlrpclib.ServerProxy("%s" % sys.argv[1])
+server = xmlrpc.client.ServerProxy("%s" % sys.argv[1])
 server.scheduler.workers.set_config("%s" % sys.argv[2], "dispatcher_ip: %s" % sys.argv[3])
