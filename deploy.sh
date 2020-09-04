d8946b8d6d98d67807f0b441e22a7a978a6a67ef
Corentin LABBE <clabbe@baylibre.com>
2020-09-04 08:38:23 +0200

    deploy.sh: Remove udev rules in a more restrictive way
    Now lava-dispatcher-host package exists, we should not remove its udev
    rules.
    So remove only only lava-docker rules.

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ BEROOT=""
 if [ $(id -u) -ne 0 ];then
 	BEROOT="sudo "
 fi
-$BEROOT rm /etc/udev/rules.d/*lava*rules
-$BEROOT cp udev/*lava*rules /etc/udev/rules.d/
+$BEROOT rm /etc/udev/rules.d/*lavaworker-udev*rules
+$BEROOT cp udev/*lavaworker-udev*rules /etc/udev/rules.d/
 $BEROOT udevadm control --reload-rules || exit $?
 $BEROOT udevadm trigger || exit $?
 
